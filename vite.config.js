8b2c6ef6315dc492b862aa7adc1e5bb00a502fc1
Rahulguguloth <rahulguguloth65@gmail.com>
2025-10-24 20:46:57 +0530

    errors fixed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    // produce source maps so runtime minified stack traces can be mapped back to source
+    sourcemap: true
+  },
 })
